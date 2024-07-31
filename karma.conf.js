21025f99def35c18a79123c3e4033217bc677a6a
Cleophas <cleophasmashiri@gmail.com>
2024-07-31 13:49:33 +0200

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -1,7 +1,7 @@
 process.env.CHROME_BIN = '/usr/bin/chromium-browser';
 module.exports = function(config) {
   config.set({
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadless'],
     // other configuration options
   });
 };
